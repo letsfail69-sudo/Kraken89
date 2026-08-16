3026ec8dcd0691086b1a6420e31a03f007bf9b07
letsfail69-sudo <letsfail69@gmail.com>
2026-08-16 17:00:59 +0200

    aktualizace 01

---
+++ js/analytics.js
@@ -0,0 +1,11 @@
+// Google Analytics 4 - Kraken89.cz
+// Measurement ID: G-G2GRQLF6MF
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+gtag('config', 'G-G2GRQLF6MF');
